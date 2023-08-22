a97480c7f8be0b0a70680610625616dd1f79d78c
Jan <janan9980@gmail.com>
2023-08-22 12:01:59 +0200

    Version 1

---
+++ CalculateWorkingDays/Feiertage.cs
@@ -0,0 +1,60 @@
+namespace CalculateWorkingDays;
+
+public class Feiertage
+{
+
+    public static List<DateTime> GetFeiertage()
+    {
+        var feiertage = new List<DateTime>();
+
+        for (int year = 2019; year <= 2023; year++)
+        {
+            feiertage.Add(new DateTime(year, 1, 1));    // Neujahr
+            feiertage.Add(new DateTime(year, 5, 1));    // Tag der Arbeit
+            feiertage.Add(new DateTime(year, 10, 3));   // Tag der dt Einheit
+            feiertage.Add(new DateTime(year, 11, 1));   // Allerheiligen
+            feiertage.Add(new DateTime(year, 12, 25));  // 1. Weihnachtstag
+            feiertage.Add(new DateTime(year, 12, 26));  // 2. Weihnachtstag
+            
+            // weitere Bewegliche Feiertage mit Osterformel
+            var osterSonntag = CalculateOstern(year);
+            var karfreitag = osterSonntag.AddDays(-1);
+            feiertage.Add(karfreitag);
+            var ostermontag = osterSonntag.AddDays(1);
+            feiertage.Add(ostermontag);
+
+            var christihimmelfahrt = osterSonntag.AddDays(40);
+            feiertage.Add(christihimmelfahrt);
+
+            var pfingstsonntag = osterSonntag.AddDays(50);
+            var pfingstmontag = osterSonntag.AddDays(51);
+            feiertage.Add(pfingstmontag);
+
+            var fronleichnam = osterSonntag.AddDays(60);
+            feiertage.Add(fronleichnam);
+        }
+
+        return feiertage;
+    }
+
+    static DateTime CalculateOstern(int year)
+    {
+            int c = year / 100;
+            int n = year - 19 * (year / 19);
+            int k = (c - 17) / 25;
+            int i = c - c / 4 - ((c - k) / 3) + 19 * n + 15;
+            i = i - 30 * (i / 30);
+            i = i - (i / 28) * ((1 - (i / 28)) * (29 / (i + 1)) * ((21 - n) / 11));
+            int j = year + (year / 4) + i + 2 - c + (c / 4);
+            j = j - 7 * (j / 7);
+            int l = i - j;
+ 
+            int easterMonth = 3 + ((l + 40) / 44);
+            int easterDay = l + 28 - 31 * (easterMonth / 4);
+
+            return new DateTime(year, easterMonth, easterDay);
+    }
+    
+}
+    
+    
---
+++ CalculateWorkingDays/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using CalculateWorkingDays;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFilePath = "/home/user/Data/Zeiten.csv";
+        string outputFilePath = "/home/user/Data/Zeiten73.csv";
+
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ","
+        };
+        
+        using (var reader = new StreamReader(inputFilePath))
+        using (var csv = new CsvReader(reader, config))
+        {
+            var eintrage = csv.GetRecords<Zeiten>();
+
+            List<DateTime> feiertagsliste = Feiertage.GetFeiertage();
+
+            using (var writer = new StreamWriter(outputFilePath))
+            using (var csvWriter = new CsvWriter(writer, config))
+            {
+                csvWriter.WriteHeader<Zeiten>();
+                csvWriter.NextRecord();
+                
+                foreach (var eintrag in eintrage)
+                {
+                    int days = CalculateWorkingDays(eintrag.StartDate, eintrag.EndDate, feiertagsliste);
+                    csvWriter.WriteField(eintrag.StartDate);
+                    csvWriter.WriteField(eintrag.EndDate);
+                    csvWriter.WriteField(days);
+                    csvWriter.NextRecord();
+                }
+            }
+        }
+    }
+
+    static int CalculateWorkingDays(DateTime startDate, DateTime endDate, List<DateTime> feiertage)
+    {
+        int weekdays = 0;
+        while (startDate <= endDate)
+        {
+            if (startDate.DayOfWeek != DayOfWeek.Saturday 
+                && startDate.DayOfWeek != DayOfWeek.Sunday && !feiertage.Contains(startDate))
+            {
+                weekdays++;
+            }
+            startDate = startDate.AddDays(1);
+        }
+        return weekdays;
+    }
+}
+
+public record Zeiten(DateTime StartDate, DateTime EndDate, int Days);
